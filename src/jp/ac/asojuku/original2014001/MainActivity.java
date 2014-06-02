0b0aaeaa9b57109c7d81bee43ac6fbae44b515ae
student <student@PC111S25.fgakusei.local>
2014-06-02 10:55:06 +0900

    karihozon

--- src/jp/ac/asojuku/original2014001/MainActivity.java
+++ src/jp/ac/asojuku/original2014001/MainActivity.java
@@ -1,8 +1,13 @@
 package jp.ac.asojuku.original2014001;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +24,36 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.btnOK:
+			EditText etv = (EditText)findViewById(R.id.edtName);
+			String inputMsg = etv.getText().toString();
+			Random rnd = new Random();
+			int ran = rnd.nextInt(4);
+			Intent intent = null;
+			switch(ran){
+				case 0:
+					intent = new Intent(MainActivity.this, DaikichiActivity.class);
+					startActivity(intent);
+					break;
+				case 1:
+					intent = new Intent(MainActivity.this, ChuukichiActivity.class);
+					startActivity(intent);
+					break;
+				case 2:
+					intent = new Intent(MainActivity.this, KyouActivity.class);
+					startActivity(intent);
+					break;
+				case 3:
+					intent = new Intent(MainActivity.this, DaikyouActivity.class);
+					startActivity(intent);
+					break;
+
+			}
+
+
+		}
+	}
 }

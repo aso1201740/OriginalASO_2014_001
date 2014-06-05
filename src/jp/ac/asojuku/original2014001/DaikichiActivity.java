4dcb295fd6de15b81f5a46a45011fad83cd2673e
student <student@PC633S10.fgakusei.local>
2014-06-05 10:34:57 +0900

    文字受け渡し

--- src/jp/ac/asojuku/original2014001/DaikichiActivity.java
+++ src/jp/ac/asojuku/original2014001/DaikichiActivity.java
@@ -1,7 +1,9 @@
 package jp.ac.asojuku.original2014001;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DaikichiActivity extends Activity {
 
@@ -10,6 +12,10 @@ public class DaikichiActivity extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.daikichi);
+		Intent intent = getIntent();
+		String name = intent.getStringExtra("naem");
+		TextView tv = (TextView)findViewById(R.id.txvMsg);
+		tv.setText(name + "さんの今日の運勢は…");
 	}
 
 }
